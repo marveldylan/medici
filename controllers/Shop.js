d43bcd036829298dadcf952edd9b700008a1b213
Dylan Marvel <marvel.dylan.james@gmail.com>
2022-03-28 16:06:38 -0500

    created Shop controller

---
+++ controllers/Shop.js
@@ -0,0 +1,14 @@
+const Shop = require('../models/shop')
+
+const getShops = async (req, res) => {
+    try {
+        const shops = await Shop.find()
+        return res.status(200).json({ shops })
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
+}
+
+module.exports = {
+    getShops
+}
