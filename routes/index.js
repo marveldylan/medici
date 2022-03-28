014209ff64ba526c8ed8b8efb63891eb89862ed6
Dylan Marvel <marvel.dylan.james@gmail.com>
2022-03-28 16:00:34 -0500

    created preliminary routes

--- routes/index.js
+++ routes/index.js
@@ -3,3 +3,13 @@ const router = Router();
 const shopController = require('../controllers/Shop')
 const reviewController = require('../controllers/Review')
 const nftController = require('../controllers/NFT')
+
+// router.get('/shops', shopController.getShops)
+
+// router.post('shops/reviews/:id', reviewController.createReview)
+// router.get('shops/reviews/:id', reviewController.getReviews)
+
+// router.post('shops/nfts/:id', nftController.createNFT)
+// router.get('shops/nfts/:id', nftController.getNFTs)
+// router.update('shops/nfts/:id', nftController.updateNFT)
+// router.delete('shops/nfts/:id', nftController.deleteNFT)
