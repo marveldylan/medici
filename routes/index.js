79e8ef87f93971dd6dfc1ebe0de36639a0e807d8
Dylan Marvel <marvel.dylan.james@gmail.com>
2022-03-28 15:55:10 -0500

    added controllers for shop, review, and nft and imported to routes index

---
+++ routes/index.js
@@ -0,0 +1,5 @@
+const { Router } = require('express');
+const router = Router();
+const shopController = require('../controllers/Shop')
+const reviewController = require('../controllers/Review')
+const nftController = require('../controllers/NFT')
