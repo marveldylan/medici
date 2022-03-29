f18a33d5af67a8bf833cbf0b1a0b1cd4801503ca
Dylan Marvel <marvel.dylan.james@gmail.com>
2022-03-29 09:17:56 -0500

    global commit

--- client/src/components/LeftSidebar.jsx
+++ client/src/components/LeftSidebar.jsx
@@ -5,11 +5,11 @@ const LeftSidebar = () => {
     return (
        <div className="left-sidebar">
            <div className="featured-header">
-
+                <p>Featured</p>
            </div>
            <Featured />
            <div className="featured-footer">
-               
+
            </div>
        </div>
     )
