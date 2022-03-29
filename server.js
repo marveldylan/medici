c8761ecc29ca2c59e25b91409372c1d47c2e93c6
Dylan Marvel <marvel.dylan.james@gmail.com>
2022-03-29 14:15:59 -0500

    pushing to Heroku

--- server.js
+++ server.js
@@ -16,14 +16,20 @@ const app = express();
 
 app.use(cors());
 
+app.use(express.static(`${__dirname}/client/build`));
+
 app.use(bodyParser.json());
 
 app.use(logger('dev'));
 
-app.use(urlencoded({required: false}))
+app.use(urlencoded({required: false}));
 
 app.use('/', routes);
 
-db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
-app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
+app.get('/*', (req, res) => {
+    res.sendFile(`${__dirname}/client/build/index.html`)
+   });
+
+app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
