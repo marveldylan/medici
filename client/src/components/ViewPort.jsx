8ceda8a40ad5d94b90f227499894dc4a1c5a51de
Dylan Marvel <marvel.dylan.james@gmail.com>
2022-03-29 13:07:17 -0500

    added default image and additional styling, along with Banner component creation

--- client/src/components/ViewPort.jsx
+++ client/src/components/ViewPort.jsx
@@ -1,10 +1,12 @@
-
+// image taken from duckduckgo search https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi1.pngguru.com%2Fpreview%2F841%2F447%2F599%2Fs-008-aesthetic-png-clipart.jpg&f=1&nofb=1
+// color-corrected and converted to svg with Gravit.io
+import defaultImage from '../assets/david-edited.svg'
 
 const ViewPort = () => {
 
     return (
         <div className="viewport">
-
+             <img class="default-image" src={ defaultImage } alt="cant find it" />
         </div>
     )
 }
